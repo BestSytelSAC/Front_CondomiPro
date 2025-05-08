fa98779c38f9e54a118941efe0ade3e1dcea3c1e
Julio Gómez <amazingcomputer2012@gmail.com>
2025-05-08 18:51:39 -0500

    first commit

--- src/pages/private/PrivateRouter.tsx
+++ src/pages/private/PrivateRouter.tsx
@@ -7,8 +7,8 @@ export const PrivateRouter = () => {
     return (
         <RoutesWithNotFound>
             <Route path="/" element={<Navigate to="dashboard"/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
-            <Route path="/propietario" element={<Propietario />}/>
+            {/* <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/propietario" element={<Propietario />}/> */}
             <Route path="/user"/>
             <Route path="/about"/>
         </RoutesWithNotFound>
